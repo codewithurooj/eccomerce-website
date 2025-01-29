2f7f5b91419efaf1aa17fd8a0142d1e31c8c66af
codewithurooj <165995978+codewithurooj@users.noreply.github.com>
2025-01-29 11:26:43 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,25 +11,20 @@ const Hero: React.FC = () => {
         <div className='flex flex-col items-center text-black md:items-start z-10'>
           <h1 className='text-6xl font-black leading-[125%] sm:text-5xl md:text-[96px]
           md:leading-[125%] '>
-                        <span className='bg-white p-1 rounded'>LET'S</span><br/>EXPLORE
+            <span className='bg-white p-1 rounded'>LET&apos;S</span><br/>EXPLORE
             <br/><span className='bg-yellow-300 p-1 rounded'>UNIQUE</span><br/>CLOTHES
-        
             <br/><span className=''></span>
-            </h1>
+          </h1>
           <p className='md:text-[32px]'>Live for Influential and Innovative fashion!</p>
-           <div className='flex flex-col md:flex-row md:items-center justify-evenly gap-9 pt-8'>
-          <button className='bg-black text-white  rounded-md py-4 px-4 text-center'>Shop Now</button>
-        </div>
-
+          <div className='flex flex-col md:flex-row md:items-center justify-evenly gap-9 pt-8'>
+            <button className='bg-black text-white rounded-md py-4 px-4 text-center'>Shop Now</button>
+          </div>
         </div>
         <div className='flex justify-center md:justify-end'>
           <Image className='w-full' src="/girl.png" alt="hero" width={400} height={400} />
         </div>
-        
       </section>
     </div>
-  
-    
   );
 };
 
